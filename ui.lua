ea34a0fd6a2984faaf257153df0fb37633d70d30
flyinh <92132985+flyinh@users.noreply.github.com>
2023-04-26 12:10:38 +0800

    Update ui.lua
    added clamping

--- ui.lua
+++ ui.lua
@@ -390,7 +390,7 @@ function visuals.round_info()
 
     local info = { }
     local j = 0
-    local l = (client.current_threat() == nil or not entity.is_alive(entity.get_local_player())) and 0 or renderer.measure_text("", entity.get_player_name(client.current_threat())) - 40
+    local l = (client.current_threat() == nil or not entity.is_alive(entity.get_local_player())) and 0 or visuals.clamp(renderer.measure_text("", entity.get_player_name(client.current_threat())) - 40, 20, renderer.measure_text("", entity.get_player_name(client.current_threat())) - 40)
 
     local bomb = entity.get_all("CPlantedC4")[1]
     
